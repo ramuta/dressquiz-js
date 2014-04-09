40d5ebbf320c0b56d33da5d20d7ecdabe481b297
Matej Ramuta <matej.ramuta@gmail.com>
2014-04-09 12:22:23 +0200

    correct/wrong answer + hide/show result text and next button

--- js/quiz.js
+++ js/quiz.js
@@ -1,11 +1,12 @@
 var ul = document.getElementById("answers");
 var rightDress;
+var nextButton = document.getElementById("next");
+var resultText = document.getElementById("result");
 
 
 window.onload = function() {
     setNewQuestion(randomDress());
 
-    var nextButton = document.getElementById("next");
     nextButton.onclick = function() {
         rightDress = null;
         setNewQuestion(randomDress());
@@ -19,6 +20,9 @@ function randomDress() {
 }
 
 function setNewQuestion(dress) {
+    nextButton.style.display = "none";
+    resultText.style.display = "none";
+
     var img = document.getElementById("dress");
     img.setAttribute("src", dress.imageUrl);
 
@@ -60,17 +64,20 @@ function setNewQuestion(dress) {
 }
 
 function answerClick(answer) {
-
     var item = document.getElementById(answer.target.id);
+
+    resultText.style.display = "block";
 
     if (!item.disabled) {
         if (answer.target.innerHTML == rightDress.brand) {
             item.setAttribute("class", "dress list-group-item list-group-item-success");
-            console.log("JAAA");
+            resultText.innerHTML = "Your answer is correct :)"
         } else {
             item.setAttribute("class", "dress list-group-item list-group-item-danger");
-            console.log("NO!");
+            resultText.innerHTML = "Sorry, wrong answer... :("
         }
+
+        nextButton.style.display = "block";
 
         item.disabled = true;
     }
