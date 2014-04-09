ab526be10cfee7626e122a1cd363943b5605b1da
Matej Ramuta <matej.ramuta@gmail.com>
2014-04-09 14:54:17 +0200

    categories

---
+++ js/categories.js
@@ -0,0 +1,17 @@
+var categories = [
+    "Accessories",
+    "Dresses",
+    "Intimates",
+    "Jackets",
+    "Jewelry",
+    "Pants",
+    "Shorts",
+    "Skirts",
+    "Suits",
+    "Sweaters",
+    "Swimwear",
+    "Sweats & Hoodies",
+    "Tops",
+    "Bags",
+    "Shoes"
+];
